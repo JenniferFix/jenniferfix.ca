19d38223f392b6f6440a2c80e23a4234762dfcc1
Jennifer Fix <codingjennifer@gmail.com>
2024-09-10 18:01:00 -0600

    initial menu

---
+++ src/components/Menu.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Menu = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Bio</li>
+        <li>Resume</li>
+        <li>Work</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Menu
